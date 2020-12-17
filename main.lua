98ba5a3d895be31c07234840d225bd5abd85b790
pakeke-constructor <62230683+pakeke-constructor@users.noreply.github.com>
2020-12-17 13:15:33 +1300

    Add files via upload

--- main.lua
+++ main.lua
@@ -5,6 +5,10 @@ _G.id = function(val) return val end
 
 
 local fennel = require("fennel.fennel")
+
+print(fennel.searcher)
+for i,v in pairs(fennel.searcher)do print("::: ", i,v) end
+
 table.insert(package.loaders or package.searchers, fennel.searcher)
 
 require("gravity")
